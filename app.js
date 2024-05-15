5ddeb8960aaccf5d215b0898f04035571f292a6b
Mauricio Ampuero <mdampuero@gmail.com>
2024-05-15 18:11:31 +0200

    Fix delay for mariadb ready

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
 require('dotenv').config();
-
-const Server = require('./models/server');
-
-const server = new Server();
-
-server.listen();
+/**
+ * Delay waiting for mariadb server to be ready
+ */
+setTimeout(() => {
+    const Server = require('./models/server');
+    const server = new Server();
+    server.listen();
+}, 3000);
